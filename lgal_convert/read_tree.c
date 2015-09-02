e0b1d96c1bce001643e35b203fabd27bee8cc7e3
Chaichalit Srisawat <boywert@gmail.com>
2015-09-03 00:53:49 +0100

    quick commit

--- lgal_convert/read_tree.c
+++ lgal_convert/read_tree.c
@@ -230,11 +230,11 @@ void create_bush(struct halo *halo,int64_t treenr) {
   struct halo *uparent;
   if((uparent = halo->uparent))
     halo = uparent;
-  printf("moving uparent for hid %" PRId64 "\n",halo->id);
+  printf("moving uparent for hid %" PRId64 "\n",halo->id_intree);
   movetree(treenr,halo->treenr);
   halo = halo->nexthalo;
   while(halo) {
-    printf("moving nexthalo for hid:%" PRId64 "\n",halo->id);
+    printf("moving nexthalo for hid:%" PRId64 "\n",halo->id_intree);
     movetree(treenr,halo->treenr);
     halo = halo->nexthalo;
   }
